fdacee1e4f2fa5ed6a94fbbedf1a0d0e09f727fc
Kamal <97207644+Kamal-21@users.noreply.github.com>
2024-02-24 12:51:11 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 def main():
     # Prompt user to enter the first number
-    num1 = float(input("Enter the first number: "))
+    num = float(input("Enter the first number: "))
 
     # Prompt user to enter the second number
     num2 = float(input("Enter the second number: "))
